de30b0a935484f793f9345f4935edd527a895efb
Gereon Kremer <gereon.kremer@gmail.com>
2025-07-29 21:17:17 +0200

    override headers to allow content from osm

--- config/config.override.php
+++ config/config.override.php
@@ -1,4 +1,19 @@
 <?php
 
 return [
+    // Predefined headers
+    // To disable a header in config.php, you can set its value to null
+    'headers'                 => [
+        'X-Content-Type-Options'  => 'nosniff',
+        'X-Frame-Options'         => 'sameorigin',
+        'Referrer-Policy'         => 'strict-origin-when-cross-origin',
+        'Content-Security-Policy' =>
+            'default-src \'self\' https://www.openstreetmap.org; '
+            . ' style-src \'self\' \'unsafe-inline\'; '
+            . 'img-src \'self\' data:;',
+        'X-XSS-Protection'        => '1; mode=block',
+        'Feature-Policy'          => 'autoplay \'none\'',
+        //'Strict-Transport-Security' => 'max-age=7776000',
+        //'Expect-CT' => 'max-age=7776000,enforce,report-uri="[uri]"',
+    ],
 ];
