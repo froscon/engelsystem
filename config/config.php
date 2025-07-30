6ca666370df58cb1afc863a248c4e108a6e3ac54
Gereon Kremer <gereon.kremer@gmail.com>
2025-07-30 21:49:03 +0200

    merge configs

--- config/config.php
+++ config/config.php
@@ -1,6 +1,9 @@
 <?php
 
 return [
+    // Hide columns in backend user view. Possible values are any sortable parameters of the table.
+    'disabled_user_view_columns' => ['freeloads', 'active', 'arrival_date', 'departure_date'],
+
     // Predefined headers
     // To disable a header in config.php, you can set its value to null
     'headers'                 => [
